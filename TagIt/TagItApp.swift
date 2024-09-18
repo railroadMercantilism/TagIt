45592d0304188978e94a7a94c51450684310b05e
James Barry <jtb07860@creighton.edu>
2024-09-18 15:39:44 -0500

    Initial Commit

---
+++ TagIt/TagItApp.swift
@@ -0,0 +1,32 @@
+//
+//  TagItApp.swift
+//  TagIt
+//
+//  Created by James Barry on 9/18/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TagItApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TagItTests/TagItTests.swift
@@ -0,0 +1,16 @@
+//
+//  TagItTests.swift
+//  TagItTests
+//
+//  Created by James Barry on 9/18/24.
+//
+
+import Testing
+
+struct TagItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
